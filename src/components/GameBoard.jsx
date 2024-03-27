6edfc68f276e5a986d7ed48fef2255ff58680b8a
Othnielonyx <othnielonyx@gmail.com>
2024-03-27 23:18:32 +0100

    first commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+export default function GameBoard({ onSelectSquare, board }) {
+ 
+    return (
+        <ol id="game-board">
+            {board.map((row, rowindex) => (
+                <li key= {rowindex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button 
+                                    onClick={() => onSelectSquare(rowindex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
+}
